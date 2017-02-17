296d16213d0ee4f9068862f2b1628be53fdc28fe
Asia <asiagold@mail.ru>
2017-02-17 13:15:04 +0200

    GmailRegistrationPage

--- src/test/java/OpenGMailTest.java
+++ src/test/java/OpenGMailTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import pageobjects.GMailHomePage;
+import pageobjects.GmailRegistrationPage;
 import pageobjects.GooglePage;
 import static org.junit.Assert.*;
 
@@ -17,6 +18,7 @@ public class OpenGMailTest {
     private WebDriver driver;
     private GooglePage googlePage;
     private GMailHomePage gmailHomePage;
+    private GmailRegistrationPage gmailRegistrationPage;
     private static final String BASE_URL = "https://www.google.com.ua/";
 
 
@@ -29,12 +31,12 @@ public class OpenGMailTest {
         driver.get(BASE_URL);
         googlePage = new GooglePage(driver);
     }
-/*
+
     @After
     public void tearDown() {
-        driver.quit();
+        //driver.quit();
     }
-*/
+
     @Test
     public void testOpenGMail() {
         googlePage.clickMailLink();
@@ -44,5 +46,22 @@ public class OpenGMailTest {
         assertEquals(gmailHomePage.getGmailHomeTitle(), driver.getTitle());
         gmailHomePage.clickCreateAccountLink();
         System.out.println(driver.getTitle());
+        gmailRegistrationPage = new GmailRegistrationPage(driver);
+        enterDataInFields();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    public void enterDataInFields() {
+        gmailRegistrationPage.typeFirstName("TestFirstName");
+        gmailRegistrationPage.typeLastName("TestLastName");
+        gmailRegistrationPage.typeGmailAddress("test");
+        gmailRegistrationPage.typePassword("test");
+        gmailRegistrationPage.typePasswordAgain("test");
+        gmailRegistrationPage.typeBirthDay("1");
+        gmailRegistrationPage.selectBirthMonth("8");
+        gmailRegistrationPage.typeBirthYear("1989");
+        gmailRegistrationPage.selectGender("Не указан");
+        gmailRegistrationPage.typeRecoveryPhoneNumber("950000000");
+        gmailRegistrationPage.typeRecoveryEmailAddress("test@gmail.com");
     }
 }
--- src/test/java/pageobjects/GmailRegistrationPage.java
+++ src/test/java/pageobjects/GmailRegistrationPage.java
@@ -1,8 +1,11 @@
 package pageobjects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 /**
  * Created by Asia on 16.02.2017.
@@ -24,5 +27,102 @@ public class GmailRegistrationPage {
     @FindBy(id = "PasswdAgain")
     private WebElement passwordAgainTextField;
 
-    //private final WebDriver driver;
+    @FindBy(id = "BirthDay")
+    private WebElement birthDayTextField;
+
+    @FindBy(id = "BirthYear")
+    private WebElement birthYearTextField;
+
+    @FindBy(id = "HiddenGender")
+    private WebElement genderDropDown;
+
+    @FindBy(id = "RecoveryPhoneNumber")
+    private WebElement phoneTextField;
+
+    @FindBy(id = "RecoveryEmailAddress")
+    private WebElement emailTextField;
+
+    private final WebDriver driver;
+    private static final String GMAIL_REG_PAGE = "Зарегистрируйтесь в Google";
+
+    public GmailRegistrationPage(WebDriver driver) {
+        if(!GMAIL_REG_PAGE.equals(driver.getTitle())) {
+            System.out.println(driver.getTitle());
+            throw new IllegalStateException("This is not a Gmail Registration Page");
+        }
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public void typeFirstName(String firstName) {
+        firstNameTextField.sendKeys(firstName);
+    }
+
+    public void typeLastName(String lastName) {
+        lastNameTextField.sendKeys(lastName);
+    }
+
+    public void typeGmailAddress(String gmailAddress) {
+        gmailAddressTextField.sendKeys(gmailAddress);
+    }
+
+    public void typePassword(String password) {
+        passwordTextField.sendKeys(password);
+    }
+
+    public void typePasswordAgain(String passwordAgain) {
+        passwordAgainTextField.sendKeys(passwordAgain);
+    }
+
+    public void typeBirthDay(String day) {
+        birthDayTextField.sendKeys(day);
+    }
+
+    public void selectBirthMonth(String month) {
+        driver.findElement(By.id(":0")).click();
+        if(month=="10") {
+            driver.findElement(By.id(":a")).click();
+        } else {
+            if(month=="11") {
+                driver.findElement(By.id(":b")).click();
+            } else {
+                if(month == "12") {
+                    driver.findElement(By.id(":c")).click();
+                } else {
+                    driver.findElement(By.id(":" + month)).click();
+                }
+            }
+        }
+    }
+
+    public void typeBirthYear(String year) {
+        birthYearTextField.sendKeys(year);
+    }
+
+    public void selectGender(String gender) {
+        driver.findElement(By.id(":d")).click();
+        if(gender=="Женский") {
+            driver.findElement(By.id(":e")).click();
+        } else {
+            if(gender=="Мужской") {
+                driver.findElement(By.id(":f")).click();
+            } else {
+                if(gender=="Другой") {
+                    driver.findElement(By.id(":g")).click();
+                } else {
+                    if(gender=="Не указан") {
+                        driver.findElement(By.id(":h")).click();
+                    }
+                }
+            }
+        }
+    }
+
+    public void typeRecoveryPhoneNumber(String phone) {
+        phoneTextField.sendKeys(phone);
+    }
+
+    public void typeRecoveryEmailAddress(String email) {
+        emailTextField.sendKeys(email);
+    }
 }
